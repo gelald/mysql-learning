cf5d1dae2811ac52402311051d7b963c8ca3e811
gelald <yb.ng@foxmail.com>
2023-04-14 11:10:18 +0800

    SpringBoot集成druid数据源与配置监控页面

---
+++ druid-datasource/src/main/java/com/github/gelald/druid/DruidDatasourceApplication.java
@@ -0,0 +1,15 @@
+package com.github.gelald.druid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author WuYingBin
+ * date: 2023/4/14
+ */
+@SpringBootApplication
+public class DruidDatasourceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DruidDatasourceApplication.class, args);
+    }
+}
---
+++ druid-datasource/src/test/java/com/github/gelald/druid/DruidDatasourceApplicationTest.java
@@ -0,0 +1,11 @@
+package com.github.gelald.druid;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author WuYingBin
+ * date: 2023/4/14
+ */
+@SpringBootTest
+public class DruidDatasourceApplicationTest {
+}
